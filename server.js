20edd684fdc6e5db55efe2aa4b1a285e1c85a65f
Call me zack <85793706+zrdouane@users.noreply.github.com>
2023-05-13 16:43:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 // const port = 3000;
-const server = require("http").Server(app);
+const server = require('http').Server(app);
 // const { v4: uuidv4 } = require("uuid");
 const { v4: uuidv4, validate: uuidValidate } = require("uuid");
 const io = require("socket.io")(server);
